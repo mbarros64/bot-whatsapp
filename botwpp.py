8eb679acee8ece81fa9a265d351c13f97574fa32
matheus <barros.matheusprestes@gmail.com>
2020-05-27 19:01:58 -0400

    add a delay between actions
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- botwpp.py
+++ botwpp.py
@@ -16,9 +16,13 @@ class WhatsAppBot:
         # <span data-icon="send" class="">
         for grupo in self.grupos: 
                  grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
+                 time.sleep(3)
                  grupo.click()
                  chat_box  =  self.driver.find_element_by_class_name('_1Plpp')
+                 time.sleep(3)
                  chat_box.click()
                  chat_box.send_keys(self.mensagem)
                  botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+                 time.sleep(3)
                  botao_enviar.click()
+                 time.sleep(5)
