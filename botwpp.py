880f2a7fe9e45bb5aff197e39ec36d624416cafb
matheus <barros.matheusprestes@gmail.com>
2020-05-27 18:40:36 -0400

    Add new method do send messages
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- botwpp.py
+++ botwpp.py
@@ -4,7 +4,12 @@ import time
 
 class WhatsAppBot:
     def __init__(self)
-    self.mensagem = "tESTE"
-    self.grupos =  [""]
-    options = webdriver.ChromeOptions()
-    options.add_argument("lan=pt-br")
+        self.mensagem = "tESTE"
+        self.grupos =  [""]
+        options = webdriver.ChromeOptions()
+        options.add_argument("lan=pt-br")
+        self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
+    
+    def EnviarMensagens(self):
+        
+     
