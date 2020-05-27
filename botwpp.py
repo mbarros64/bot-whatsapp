6637fd9446781e91542680fbf38b15a735a13004
matheus <barros.matheusprestes@gmail.com>
2020-05-27 18:38:37 -0400

    add class main
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- botwpp.py
+++ botwpp.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 import time 
+
+
+class WhatsAppBot:
+    def __init__(self)
+    self.mensagem = "tESTE"
+    self.grupos =  [""]
+    options = webdriver.ChromeOptions()
+    options.add_argument("lan=pt-br")
