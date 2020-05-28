5e2049cb45fa54a922d2abcfb472db9707fd1462
matheus <barros.matheusprestes@gmail.com>
2020-05-28 16:46:30 -0400

    making the bot more generic
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- botwpp.py
+++ botwpp.py
@@ -6,14 +6,11 @@ import time
 
 class WhatsAppBot:
     def __init__(self):
-        self.mensagem = "wirley eh gay"
-        self.grupos_ou_pessoas =  ["POLÍCIA MALUCO"]
+        self.mensagem = ""]
+        self.grupos_ou_pessoas =  [""]
         options = webdriver.ChromeOptions()
         options.add_argument("lan=pt-br")
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver',  options=options)
-        # <span dir="auto" title="POLÍCIA MALUCO" class="_1wjpf _3NFp9 _3FXB1">POLÍCIA MALUCO</span>#
-        # <div tabindex="-1" class="_1Plpp">#
-        # <span data-icon="send" class="">#
     def EnviarMensagens(self):
             self.driver.get('https://web.whatsapp.com')
             time.sleep(30)
@@ -22,7 +19,7 @@ class WhatsAppBot:
                      f"//span[@title='{grupo_ou_pessoa}']")
                 time.sleep(3)
                 campo_grupo.click()
-                chat_box = self.driver.find_element_by_class_name('_1wjpf')
+                chat_box = self.driver.find_element_by_class_name('')
                 time.sleep(3)
                 chat_box.click()
                 chat_box.send_keys(self.mensagem)
